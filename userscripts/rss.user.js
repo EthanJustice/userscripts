e4d9c923a97667505baf769c32cfaa4df0847cb2
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-08 19:17:46 -0400

    feat: rss notifier userscrpt

---
+++ userscripts/rss.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name     RSS Notifier
+// @description  Notifier that site contains RSS feed hints
+// @version  0.0.1
+// @grant    none
+// @run-at   document-idle
+// ==/UserScript==
+let feeds = false;
+if (document.head.querySelector('link[type="application/atom+xml')) { feeds = true }
+
+if (feeds == true) {
+	let newElement = document.createElement('p');
+	newElement.style = `
+		background: crimson;
+		color: black;
+		position: absolute;
+		width: 10%;
+		padding: 10px;
+		top: 2%;
+		left: 2%;
+		z-index: 99999;
+		text-align: center;
+		font-size: 20pt;
+	`
+	newElement.innerText = 'Feeds';
+	document.body.insertBefore(newElement, document.body.firstChild);
+
+	setTimeout(() => newElement.remove(), 3000);
+}
