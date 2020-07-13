056e491ff954f46e4828e84d3f66106e9799acd6
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-13 18:36:17 -0400

    fix(link-src-wrapper): linking to itself on image url
    Script was linking to itself when on it's own page (as in the .png/.jpeg link)

--- userscripts/link-src-wrapper.user.js
+++ userscripts/link-src-wrapper.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name     Image Link Wrapper
 // @description Wrap images in a link pointing to their source
-// @version  0.0.1
+// @version  1
 // @grant    none
 // @run-at   document-idle
 // ==/UserScript==
+let l = window.location.href;
+if (l.endsWith('png') || l.endsWith('jpeg')) { return }
 document.body.querySelectorAll('img[src]').forEach(item => {
     let element = document.createElement('a');
     element.href = item.src;
