4c70c1d60d36110057ee51bcb530be919288c9a9
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-07-13 18:30:41 -0400

    fix: refactor image dimmer

--- userscripts/image-dimmer.user.js
+++ userscripts/image-dimmer.user.js
@@ -4,12 +4,12 @@
 // @run-at   document-idle
 // @grant    none
 // ==/UserScript==
-const images = document.getElementsByTagName('img');
-for (let i = 0; i < images.length; i++) {
-    images[i].style.opacity = '0.4';
-    images[i].style.transition = '0.1s linear';
-    images[i].addEventListener('mousemove', function() { increaseOpacity(images[i]) })
-    images[i].addEventListener('mouseleave', function() { decreaseOpacity(images[i]) })
-}
+document.body.querySelectorAll('img[src]').forEach(item => {
+    item.style.opacity = '0.4';
+    item.style.transition = '0.1s linear';
+    item.addEventListener('mousemove', function () { increaseOpacity(item) })
+    item.addEventListener('mouseleave', function () { decreaseOpacity(item) })
+});
+
 function increaseOpacity(elem) { elem.style.opacity = '1' };
 function decreaseOpacity(elem) { elem.style.opacity = '0.4' };
