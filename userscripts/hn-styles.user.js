13fa7d623e844df4ae3d09e468e0489958e0462b
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-11-13 17:44:01 -0500

    chore(userscripts/hn): switch to simpler styling for HN

--- userscripts/hn-styles.user.js
+++ userscripts/hn-styles.user.js
@@ -1,91 +1,84 @@
 // ==UserScript==
 // @name     HackerNews Styling
 // @description  Styling for Y-Combinator's HackerNews
-// @version  0.0.3
-// @run-at   document-start
+// @version  0.0.4
+// @run-at   document-idle
 // @grant    none
 // @match 	 https://news.ycombinator.com/*
 // ==/UserScript==
-document.body.style.cssText = `
+let n = document.createElement("style");
+n.innerText = `
+	body {
     margin: 0;
     background: #212121;
-`
+	}
 
-document.getElementsByTagName('tbody')[0].getElementsByTagName('table')[0].style.cssText = `
-    background: #cb4b16;
-`
+	* {
+		background: #212121 !important;
+	}
 
-document.getElementsByTagName('center')[0].style.cssText = `
-    width: 100%;
-    background: #212121;
-`
+	table tbody {
+      background: #cb4b16;
+	}
 
-document.getElementById('hnmain').style.cssText = `
-    width: 90%;
-    background: #212121;
-`
+	center {
+      width: 100%;
+      background: #212121;
+	}
 
-const athings = document.getElementsByClassName('athing');
-for (let i = 0; i < athings.length; i++) {
-    athings[i].style.cssText = `
-        background: #333;
-        width: 1190px;
-    `
-};
+	#hnmain {
+      width: 90%;
+      background: #212121;
+	}
 
-const links = document.getElementsByTagName('a');
-for (let f = 0; f < links.length; f++) {
-    links[f].addEventListener('mousemove', function() { increaseOpacity(links[f]) });
+	.athing {
+		background: #333;
+		width: 1190px;
+	}
+	
+	a, a.storylink, .c00 a, .c00 a:link {
+		color: #2965e5;
+		font-size: 10pt;
+	}
 
-    links[f].addEventListener('mouseleave', function() {
-        links[f].style.opacity = `0.7`;
-    });
+	a:visited {
+		color: #16c581;
+	}
 
-    links[f].style.cssText = `
-        color: #fafafa;
-        opacity: 0.7;
-				font-size: 10pt;
-    `
-}
+	.subtext {
+		background: #333;
+		width: 1200px;
+		font-size: 4pt;
+	}
 
-const subtexts = document.getElementsByClassName('subtext');
-for (let z = 0; z < subtexts.length; z++) {
-    subtexts[z].style.cssText = `
-        background: #333;
-        width: 1200px;
-				font-size: 4pt;
-    `
-}
+	.yclinks a {
+		color: #f5f5f5
+	}
 
-const yclinksLinks = document.getElementsByClassName('yclinks')[0].getElementsByTagName('a');
-for (let e = 0; e < yclinksLinks; e++) {
-    yclinksLinks[e].style.cssText = `
-        color: #f5f5f5
-    `
-}
+	input {
+		background: #333;
+		border: none;
+		width: 20%;
+		color: #fafafa;
+		opacity: 0.7;
+	}
 
-const search = document.getElementsByTagName('input')[0];
+	input:hover { opacity: 1 }
 
-search.style.cssText = `
-    background: #333;
-    border: none;
-    width: 20%;
-    color: #fafafa;
-    opacity: 0.7;
-`
+	.commtext {
+		color: #fafafa;
+	}
 
-search.addEventListener('mousemove', function() { increaseOpacity(search) })
+	textarea, td form, a[href^="reply"] {
+		display: none !important;
+	}
 
-const commtexts = document.getElementsByClassName('commtext');
-for (let o = 0; o < commtexts.length; o++) {
-    commtexts[o].style.cssText = `
-        color: #fafafa;
-    `
-}
-
-function increaseOpacity(element) { element.style.opacity = '1' }
-
-const elements = document.getElementsByTagName('*');
-for (let p = 0; p < elements.length; p++) {
-    elements[p] == document.body || elements[p] == document.documentElement || elements[p] == document.head || elements[p] == center ? null : elements[p].style.transition = `0.1s linear`
-}
+	.comment-tree table {
+		border-top: 2px solid #404040;
+		width: 100%;
+		margin: 1% 0;
+	}
+`;
+document.head.appendChild(n);
+// removes the top banner
+document.querySelector("tr").remove();
