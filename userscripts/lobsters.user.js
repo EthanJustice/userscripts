f94c3a0dea887acc6f6b415dd820ed9bd78e36a1
EthanJustice <47954690+EthanJustice@users.noreply.github.com>
2020-11-13 17:46:09 -0500

    feat(userscripts): add lobste.rs styling

---
+++ userscripts/lobsters.user.js
@@ -0,0 +1,23 @@
+// ==UserScript==
+// @name     lobste.rs styling
+// @version  0.0.1
+// @grant    none
+// @run-at   document-end
+// @match    https://lobste.rs/*
+// ==/UserScript==
+let style = document.createElement("style");
+style.innerText = `
+body {
+    background-color: #212121;
+    color: #fafafa;
+  }
+
+  a {
+    color: #2965e5;
+	}
+
+  a:visited {
+		color: #16c581 !important;
+  }
+`;
+document.head.appendChild(style);
